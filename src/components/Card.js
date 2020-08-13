457d71fcf73b5ec3a6b7a5dd111bb9568ea6e0b7
kaustubh <kaustubh229@gmail.com>
2020-08-14 01:49:44 +0530

    AOS Added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init();
 
 const Card = ( {post, toRemove, Id} ) => {
     
@@ -7,7 +11,7 @@ const Card = ( {post, toRemove, Id} ) => {
     }
 
     return (
-        <div className="card">
+        <div data-aos="flip-right" data-aos-offset="200" data-aos-easing="ease-out" data-aos-duration="500" data-aos-once="true" className="card">
             <img className="img" alt={post.title} title={post.title} src={post.url}></img>
             <div><h1>{post.title}</h1>
             <button onClick={(e) => {remove(post, Id)}}><i className="fa fa-times-circle"></i></button></div>
