e40ff107af9cf25238192f8f6a34f916cf8d7ae8
kaustubh <kaustubh229@gmail.com>
2020-08-29 20:49:17 +0530

    UI improved of add-image. Info boxed added

--- src/components/Form.js
+++ src/components/Form.js
@@ -20,10 +20,22 @@ const Form = ({toAppend}, Id) => {
     return (
             <center>
             <form onSubmit={formSubmitted}>
-            <input className="form-input" type="text" name="des" placeholder="Enter a Suitable Title" mandatory />
-            <input className="form-input" type="text" name="url" placeholder="Enter the URL of the image" mandatory />
+            <input className="form-input" type="text" name="des" placeholder="Enter a Suitable Title" autocomplete="off" mandatory />
+            <input className="form-input" type="text" name="url" placeholder="Enter the URL of the image" autocomplete="off" mandatory />
             <button className="form-button">Submit</button>
             </form>
+            <div class="info1 container-cards">
+                <p>Please do care that the URL you enter is of the image and not of the page. This program cannot extract images form a webpage so do take care.</p>
+                <i class="info-icon fa fa-info-circle"></i>
+            </div>
+            <div class="info2 container-cards">
+                <p>The image you upload will be visible to every user across the globe so remember to pay attention of what image and description you post</p>
+                <i class="info-icon fa fa-info-circle"></i>
+            </div>
+            <div class="info3 container-cards">
+                <p>Delete the images very carefully because you will delete them for every user across the globe and you can only add them back with their URLs</p>
+                <i class="info-icon fa fa-info-circle"></i>
+            </div>
             </center>
     )
 }
