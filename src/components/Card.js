a1b7b3bb59b4d85228e6256212207598c33fef9d
kaustubh <kaustubh229@gmail.com>
2020-09-20 03:43:46 +0530

    Error resolved

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,10 +11,10 @@ const Card = ( {post, toRemove, Id} ) => {
     }
 
     return (
-        <div data-aos="flip-right" data-aos-offset="200" data-aos-easing="ease-out" data-aos-duration="500" data-aos-once="true" className="card">
+        <div data-aos="flip-right" data-aos-easing="ease-out" data-aos-duration="500" data-aos-once="true" className="card">
             <img className="img" alt={post.title} title={post.title} src={post.url}></img>
             <div><h1>{post.title}</h1>
-                <button><a href={post.url}></a><i className="fa fa-external-link-alt"></i></button>
+                <a target="blank" href={post.url}><button><i className="fa fa-external-link-alt"></i></button></a>
             <button onClick={(e) => {remove(post, Id)}}><i className="fa fa-times-circle"></i></button></div>
         </div>
     )
